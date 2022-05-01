b846c54c898d5f52a4a81f1a5b27e352fc4a2eb3
Skye Jonke <skye@skyenet.online>
2022-05-01 17:26:00 -0400

    Manga list added

---
+++ site/mangalist.php
@@ -0,0 +1,44 @@
+<?php include __DIR__ . '/scripts/list_manga.php';
+?>
+
+<html>
+
+<head>
+    <title> Manga List</title>
+</head>
+
+<body>
+
+    <table>
+        <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Original Title</th>
+            <th>Author</th>
+            <th>Description</th>
+            <th>Number Chapters</th>
+        </tr>
+        <?php
+
+        $mangas = (getSqlRows($db, $MANGA_TABLE));
+
+        while ($manga = mysqli_fetch_array($mangas)) {
+            $author = getSqlRowFromId($db, $AUTHOR_TABLE, $manga['author_id']);
+            // This should link to the author
+        ?>
+            <tr>
+                <td> <img src=content/<?= $manga['image_link'] ?>></img> </td>
+                <td> <?= $manga['title'] ?> </td>
+                <td> <?= $manga['original_title'] ?> </td>
+                <td> <a href="author.php?author_id=<?=$author['id']?>"> <?= $author['name'] ?> </a></td>
+                <td> <?= $manga['description'] ?> </td>
+                <td> <?= $manga['num_chapters'] ?> </td>
+
+            <?php
+            $html_table = $html_table . '</tr>';
+        }
+            ?>
+    </table>
+</body>
+
+</html>
