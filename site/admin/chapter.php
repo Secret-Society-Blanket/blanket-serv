1711e513be4c07de495ec324a139e3aa325a5f38
Skye Jonke <skye@skyenet.online>
2022-05-16 17:01:18 -0400

    Made the chapter feedback better match the others.

--- site/admin/chapter.php
+++ site/admin/chapter.php
@@ -2,7 +2,7 @@
 ?>
 
 <html>
-    
+
 <head>
     <title>
         Create Chapter </title>
@@ -10,44 +10,45 @@
 </head>
 
 <body>
-    <form action="/scripts/addChapter.php" method="POST" enctype="multipart/form-data" autocomplete="off">
-        <label for="manga-id"> Manga: </label>
-        <select id="manga-id" name="manga-id" />
-        <option value='-1'> Choose a Manga </option>
-        <?= $mangas ?>
-        </select><br>
-        <label for="number"> Chapter Number: </label>
-        <input type="number" id="number" name="number" /><br>
-        <label for="file"> Upload Files: </label>
-        <input type="file" id="file" name="file" /><br>
-        <label for="release-date"> Release Date: </label>
-        <input type="date" id="release-date" name="release-date" /><br>
-        <label for="credits"> Scanlation Credits: </label><br>
-        <textarea id="credits" name="credits" /></textarea><br>
-        <input type="submit" id="submit" value="Submit" /><br>
-    </form>
-    <script>
-        $('#manga-id').change(function() {
-            if ($('#manga-id').val() != -1) {
-                $.get("/scripts/getId.php", {
-                    'manga-id': $("#manga-id").val()
-                }, function(data) {
-                    let row = data;
-                    let maxNum;
-                    let minNum = 0;
-                    if (!row.isOneshot) {
-                        maxNum = row.num_chapters + 1;
-                    } else {
-                        maxNum = 1;
-                        minNum = 1;
-                    }
-                    $("#number").val(maxNum);
-                    $("#number").attr('max', maxNum);
-                    $("#number").attr('min', minNum);
-                }, 'json').done();
-            }
-        });
-    </script>
+    <form action="<?php $_PHP_SELF ?>" method="POST" enctype="multipart/form-data" autocomplete="off">
+            <label for="manga-id"> Manga: </label>
+            <select id="manga-id" name="manga-id" />
+            <option value='-1'> Choose a Manga </option>
+            <?= $mangas ?>
+            </select><br>
+            <label for="number"> Chapter Number: </label>
+            <input type="number" id="number" name="number" /><br>
+            <label for="file"> Upload Files: </label>
+            <input type="file" id="file" name="file" /><br>
+            <label for="release-date"> Release Date: </label>
+            <input type="date" id="release-date" name="release-date" /><br>
+            <label for="credits"> Scanlation Credits: </label><br>
+            <textarea id="credits" name="credits" /></textarea><br>
+            <input type="submit" id="submit" value="Submit" /><br>
+        </form>
+        <script>
+            $('#manga-id').change(function() {
+                if ($('#manga-id').val() != -1) {
+                    $.get("/scripts/getId.php", {
+                        'manga-id': $("#manga-id").val()
+                    }, function(data) {
+                        let row = data;
+                        let maxNum;
+                        let minNum = 0;
+                        if (!row.isOneshot) {
+                            maxNum = row.num_chapters + 1;
+                        } else {
+                            maxNum = 1;
+                            minNum = 1;
+                        }
+                        $("#number").val(maxNum);
+                        $("#number").attr('max', maxNum);
+                        $("#number").attr('min', minNum);
+                    }, 'json').done();
+                }
+            });
+        </script>
+        <p><?= $command_result ?> </p>
 
 </body>
 
--- site/scripts/new_chapter.php
+++ site/scripts/new_chapter.php
@@ -35,4 +35,43 @@ while ($manga = mysqli_fetch_array($results)) {
     $mangas = $mangas."</option>";
 }
 
-?>
+$command_result = "";
+if ($_POST) {
+    $command_result = "Something went wrong...";
+    $manga = getSqlRowFromId($db, $MANGA_TABLE, $_POST['manga-id']);
+    $insert_c_q = "INSERT INTO {$CHAPTER_TABLE} (manga_id, path, number, release_date , credits) VALUES (?, ?, ?, ?, ?)";
+    $prep = mysqli_prepare($db,
+        $insert_c_q
+    );
+    mysqli_stmt_bind_param($prep, 'isiss', $mangaid, $path, $number, $releasedate, $credits);
+    if ($manga == NULL) {
+        $command_result = 'No Manga Found';
+    } else {
+        $mangaid = $_POST['manga-id'];
+        $path = saveChapter($_FILES['file']);
+        if ($_POST["release-date"] = "") {
+            $releasedate = $_POST["release-date"];
+        } else {
+            $cdate = new DateTime();
+            $releasedate = $cdate->format("Y-m-d H:i:s");
+        }
+        $credits = $_POST['credits'];
+        $number = $_POST['number'];
+
+        if (!$manga['is_oneshot']) {
+            $insert_m_q = "UPDATE {$MANGA_TABLE} SET num_chapters = ? WHERE id = ?";
+            $prepm = mysqli_prepare(
+                $db,
+                $insert_m_q
+            );
+            mysqli_stmt_bind_param($prepm, 'ii', $m_num, $m_id);
+            $m_num = $number;
+            $m_id = $mangaid;
+            mysqli_stmt_execute($prepm);
+        }
+        if (mysqli_stmt_execute($prep)) {
+            $command_result = 'Complete!';
+        }
+    }
+}
+
