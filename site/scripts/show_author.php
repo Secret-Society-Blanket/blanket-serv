89bf0e9a1e2fa7b6a65de46fccbfa40769730242
Skye Jonke <skye@skyenet.online>
2022-06-19 14:16:46 -0400

    Solved error on author page

--- site/scripts/show_author.php
+++ site/scripts/show_author.php
@@ -4,7 +4,11 @@ $author;
 $mangas;
 $db = getSqli();
 if ($_GET) {
-    if (isset($_GET['id'])) {
+    if (isset($_GET['id']) or isset($_GET['author_id'])) {
+        if (!isset($_GET['id'])) {
+            $_GET['id'] = $_GET['author_id'];
+        }
+
         $author = getSqlRowFromId($db, $AUTHOR_TABLE, $_GET['id']);
         $mangas = mysqli_query($db, "SELECT * FROM $MANGA_TABLE where author_id = {$author['id']}");
     }
